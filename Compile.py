ce76d971523849eed8d52dfd285cd55f39bc971f
Jubreocta <oyeleyemojuba@gmail.com>
2023-12-04 19:09:27 +0000

    re organise structure

--- Compile.py
+++ Compile.py
@@ -1,6 +1,7 @@
 from UoW.Connectors import Wrangler
 
-#For NBA br data: compile many seasons from raw folder into 1 csv
-#then put 1 file named Raw Data in Data folder
-W = Wrangler()
-W.write_csv_to_folder(W.many_seasons_to_1_file("raw"), "Data/Raw Data")
+if __name__ == "__main__":
+    #For NBA br data: compile many seasons from raw folder into 1 csv
+    #then put 1 file named Raw Data in Data folder
+    W = Wrangler()
+    W.write_csv_to_folder(W.many_seasons_to_1_file("raw"), "Data/Raw Data")
